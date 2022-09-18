4b713fb3d9cf58b2b9581379117bbda560522fcd
musangisilvia <musangisilvia@gmail.com>
2022-09-18 19:37:11 +0300

    [Fix] round up

--- Task2.py
+++ Task2.py
@@ -1,3 +1,6 @@
+import math
+
+# test input
 Baskets = {
     1:[{
         
@@ -43,8 +46,8 @@ Baskets = {
     }]
 }
 
-def round_nearest(num, precision):
-    return round(math.ceil(num / precision) - 0.05, 1) * precision
+def round_nearest(num):
+    return math.ceil(num * 100) / 100
 
 def exclude_items(item):
     excluded = ['book', 'chocolate', 'headache pills']
@@ -67,38 +70,42 @@ def print_receipt(basket):
     for item in basket:
         #  Check the kind of item
         prod = item['prod']
-        res = exclude_items(prod)
+        qty = item['qty']
+        res = exclude_items(prod) # true or false
 
         if prod.__contains__('imported'):
-            if res is True:
+            if res is True: #if item is imported food/book/medicine
                 duty = import_duty * item['price']
-                price = duty + item['price']
-                total_sales_tax += duty
-                total_price += price
-                print(f"{prod}: {price}")
+                price = round_nearest(duty) + item['price']
+                total_sales_tax += round_nearest(duty)
+                total_price += round_nearest(price)
+                total_price += round_nearest(price)
+                print(f"{qty} {prod}: {round_nearest(price)}")
             else:
                 sales_tax = (basic_tax * item['price'])/100
                 duty = import_duty * item['price']
-                price = duty + sales_tax + item['price']
-                total_sales_tax += duty + sales_tax
-                total_price += price
-                print(f"{prod}: {price}")
+                price = round_nearest(duty) + round_nearest(sales_tax) + item['price']
+                total_sales_tax += round_nearest(duty) + round_nearest(sales_tax)
+                total_price += round_nearest(price)
+                print(f"{qty} {prod}: {round_nearest(price)}")
 
         else:
                 if res is True:
                     price = item['price']
-                    total_price += price
-                    print(f"{prod}: {price}")
+                    total_price += round_nearest(price)
+                    print(f"{qty} {prod}: {round_nearest(price)}")
                 else:
                     sales_tax = (basic_tax * item['price'])/100
-                    price = sales_tax + item['price']
-                    total_price += price
-                    total_sales_tax += sales_tax
-                    print(f"{prod}: {price}")
+                    price = round_nearest(sales_tax) + item['price']
+                    total_price += round_nearest(price)
+                    total_sales_tax += round_nearest(sales_tax)
+                    print(f"{qty} {prod}: {round_nearest(price)}")
 
+        print(total_price)
 
-        # res = exclude_items(item['prod'])
-        # print(f"{prod}: {res}")
-    print(f"Total Sales Tax: {total_sales_tax}")
-    print(f"Total Price: {total_price}")
-print_receipt(Baskets[2])
+    print(f"Sales Tax: {round_nearest(total_sales_tax)}")
+    print(f"Price: {round_nearest(total_price)}")
+
+# print_receipt(Baskets[1])
+# print_receipt(Baskets[2])
+print_receipt(Baskets[3])
