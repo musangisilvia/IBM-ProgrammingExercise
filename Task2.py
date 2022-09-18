6d1d3c971fc05fcdc2c21b678e7f0870a1a34b82
musangisilvia <musangisilvia@gmail.com>
2022-09-18 17:16:09 +0300

    use 'any()' to look for excluded products

--- Task2.py
+++ Task2.py
@@ -43,11 +43,62 @@ Baskets = {
     }]
 }
 
-def print_Receipt(basket):
+def round_nearest(num, precision):
+    return round(math.ceil(num / precision) - 0.05, 1) * precision
+
+def exclude_items(item):
+    excluded = ['book', 'chocolate', 'headache pills']
+    return any(it in item for it in excluded)
+
+
+def print_receipt(basket):
     """
         Prints the Receipt in the format:
             Item Price
             Sales Tax
             Total Price
     """
+    total_sales_tax = 0
+    total_price = 0
+    basic_tax = 10 # 10%
+    import_duty = 0.05 # 5%
+
     
+    for item in basket:
+        #  Check the kind of item
+        prod = item['prod']
+        res = exclude_items(prod)
+
+        if prod.__contains__('imported'):
+            if res is True:
+                duty = import_duty * item['price']
+                price = duty + item['price']
+                total_sales_tax += duty
+                total_price += price
+                print(f"{prod}: {price}")
+            else:
+                sales_tax = (basic_tax * item['price'])/100
+                duty = import_duty * item['price']
+                price = duty + sales_tax + item['price']
+                total_sales_tax += duty + sales_tax
+                total_price += price
+                print(f"{prod}: {price}")
+
+        else:
+                if res is True:
+                    price = item['price']
+                    total_price += price
+                    print(f"{prod}: {price}")
+                else:
+                    sales_tax = (basic_tax * item['price'])/100
+                    price = sales_tax + item['price']
+                    total_price += price
+                    total_sales_tax += sales_tax
+                    print(f"{prod}: {price}")
+
+
+        # res = exclude_items(item['prod'])
+        # print(f"{prod}: {res}")
+    print(f"Total Sales Tax: {total_sales_tax}")
+    print(f"Total Price: {total_price}")
+print_receipt(Baskets[2])
