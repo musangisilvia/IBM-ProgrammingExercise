462f3724f9e65bcb065dcf95d662cc601c09ba9e
musangisilvia <musangisilvia@gmail.com>
2022-09-18 14:03:55 +0300

    add function

---
+++ Task2.py
@@ -0,0 +1,53 @@
+Baskets = {
+    1:[{
+        
+        "qty": 1,
+        "prod": "book",
+        "price": 12.49
+    },{
+        
+        "qty": 1,
+        "prod": "music CD",
+        "price": 14.99
+    },{
+        
+        "qty": 1,
+        "prod": "chocolate bar",
+        "price": 0.85
+    }],
+    2: [{
+        "qty": 1,
+        "prod": "imported box of chocolates",
+        "price": 10.00
+    }, {
+        "qty": 1,
+        "prod": "imported bottle of perfume",
+        "price": 47.50
+    }],
+    3: [{
+        "qty": 1,
+        "prod": "imported bottle of perfume",
+        "price": 27.99
+    },{
+        "qty": 1,
+        "prod": "bottle of perfume",
+        "price": 18.99
+    },{
+        "qty": 1,
+        "prod": "packet of headache pills",
+        "price": 9.75
+    },{
+        "qty": 1,
+        "prod": "box of imported chocolates",
+        "price": 11.25
+    }]
+}
+
+def print_Receipt(basket):
+    """
+        Prints the Receipt in the format:
+            Item Price
+            Sales Tax
+            Total Price
+    """
+    
